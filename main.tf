3026e5acf4505523288d047d1b68739bb374a3cf
Pawel Fiuto <pawel@bashton.com>
2017-02-07 03:04:48 +0000

    Add policy allowing decrypt using KMS

--- main.tf
+++ main.tf
@@ -349,3 +349,26 @@ resource "aws_iam_role_policy_attachment" "ssm_managed" {
 
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
+
+# KMS read policy document
+data "aws_iam_policy_document" "kms_read_policy" {
+  statement {
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey"
+    ]
+    effect = "Allow"
+    resources = ["${split(",", var.kms_arns)}"]
+  }
+}
+
+# KMS read IAM role policy
+resource "aws_iam_role_policy" "read_kms" {
+  name = "read_kms"
+  count = "${var.kms_decrypt}"
+  role  = "${aws_iam_role.default_role.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  policy = "${data.aws_iam_policy_document.kms_read_policy.json}"
+}
