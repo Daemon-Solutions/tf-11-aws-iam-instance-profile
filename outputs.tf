dd9575e2693c61dfd66754c9f9ddfe35c4a6e2ed
Pawel Fiuto <pawel@bashton.com>
2017-02-07 03:05:04 +0000

    More outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,27 @@
 output "profile_id" {
   value = "${aws_iam_instance_profile.instance_profile.id}"
 }
+
+output "profile_arn" {
+  value = "${aws_iam_instance_profile.instance_profile.arn}"
+}
+
+output "profile_name" {
+  value = "${aws_iam_instance_profile.instance_profile.name}"
+}
+
+output "profile_create_date" {
+  value = "${aws_iam_instance_profile.instance_profile.create_date}"
+}
+
+output "profile_path" {
+  value = "${aws_iam_instance_profile.instance_profile.path}"
+}
+
+output "profile_roles" {
+  value = "${aws_iam_instance_profile.instance_profile.roles}"
+}
+
+output "profile_unique_id" {
+  value = "${aws_iam_instance_profile.instance_profile.unique_id}"
+}
