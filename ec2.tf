38976fb834301ec67bad736b98c975cdc920313c
James Hollins <james.hollins@claranet.uk>
2018-11-07 09:21:11 +0000

    TF: Added assign private ip iam role policy

--- ec2.tf
+++ ec2.tf
@@ -126,3 +126,28 @@ resource "aws_iam_role_policy" "ec2_write_tags" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy" "ec2_assign_private_ip" {
+  name  = "ec2_assign_private_ip"
+  count = "${var.ec2_assign_private_ip}"
+  role  = "${aws_iam_role.default_role.id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:AssignPrivateIpAddresses"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
